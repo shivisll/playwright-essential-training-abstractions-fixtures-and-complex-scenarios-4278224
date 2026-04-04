c9ddcf8223044a93dd2d317357c44c19da4e2108
shivisll <llorente.1b@gmail.com>
2026-04-03 22:15:36 -0600

    course complete

--- tests/bagel-shop/test.spec.ts
+++ tests/bagel-shop/test.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from "@playwright/test";
 
 test("Bagel Shop Smoke Test", async ({ page }) => {
+  test.setTimeout(120_000);
   await page.goto("http://localhost:5173");
 
   await expect(
-    page.getByText("The best bagels in town since 1995!")
+    page.getByText("The best bagels in town since 1995!"),
   ).toBeVisible();
 });
